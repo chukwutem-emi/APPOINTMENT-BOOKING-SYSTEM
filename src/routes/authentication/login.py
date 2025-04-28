01051e4a35e98116bdeb572d9a5c10414bdff664
chukwutem-emi <chukwutememi@gmail.com>
2025-04-28 16:34:15 +0100

    updated

--- src/routes/authentication/login.py
+++ src/routes/authentication/login.py
@@ -32,12 +32,15 @@ def sign_in():
                 user_info=connection.execute(statement=user_login, parameters={"email_address":email_address})
                 user=user_info.first()
                 print(f"user { user}")
-                if not user or not check_password_hash(user.password, password=password):
+                password_hashed=user["password"]
+                public_id=user["public_id"]
+                username=user["username"]
+                if not user or not check_password_hash(pwhash=password_hashed, password=password):
                     return jsonify({"verification":"We could not verify your details!. Please check your input or signup if you haven't registered"}), 400
-                token=jwt.encode({"public_id":user.public_id, "exp":datetime.datetime.now(tz=datetime.timezone.utc)+datetime.timedelta(minutes=60)}, current_app.config["SECRET_KEY"])
+                token=jwt.encode({"public_id":public_id, "exp":datetime.datetime.now(tz=datetime.timezone.utc)+datetime.timedelta(minutes=60)}, current_app.config["SECRET_KEY"])
 
                 subject = "Login Alert!"
-                body = f"Hi @{user.username}, You just signed-in into your account!.\n\nPlease if you are not the one that signed-in,\nthen contact-us at: chukwutememi@gmail.com,\n\nBest regard!\nThe Team"
+                body = f"Hi @{username}, You just signed-in into your account!.\n\nPlease if you are not the one that signed-in,\nthen contact-us at: chukwutememi@gmail.com,\n\nBest regard!\nThe Team"
                 receiver = email_address
                 send_mail(subject=subject, body=body, receiver=receiver) 
 
