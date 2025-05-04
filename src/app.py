e6e7bb15f3f6e16032a63b2aa7efeb987f84b5ff
chukwutem-emi <chukwutememi@gmail.com>
2025-05-04 22:33:17 +0100

    updated

--- src/app.py
+++ src/app.py
@@ -20,13 +20,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = base_uri
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-app.config["MAIL_SERVER"] = "smtp-relay.brevo.com"
-app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
+app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "True") == "True"    
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
-app.config["MAIL_USERNAME"] = "chukwutememi@gmail.com"
-app.config["MAIL_TLS"] = True
-app.config["MAIL_SSL"] = False
-app.config["MAIL_DEFAULT_SENDER"] = "chukwutememi@gmail.com"
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_SSL"] = os.getenv("MAIL_SSL", "False") == "False"
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
 
 mail.init_app(app=app)
 
