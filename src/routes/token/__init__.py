d1eddfc9198f3b38eac33443d5d6afd14aaba655
chukwutem-emi <chukwutememi@gmail.com>
2025-07-05 17:33:20 +0100

    enpoint for clearing token created

---
+++ src/routes/token/__init__.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from routes.token.clearGoogleToken import clear_google_token
+from flask_cors import CORS
+
+token_bp = Blueprint("token", __name__, url_prefix="/token")
+CORS(
+    token_bp,
+    origins=["http://localhost:1234"],
+    methods=["POST", "GET", "DELETE", "PUT", "OPTIONS"],
+    allow_headers=["Authorization", "access-token", "Content-Type"],
+    supports_credentials=True
+)
+
+token_bp.add_url_rule(rule="/clear-token", endpoint="/clear-token", view_func=clear_google_token, methods=["PUT"])
---
+++ src/routes/token/clearGoogleToken.py
@@ -0,0 +1,24 @@
+from routes.authentication.accessToken import token_required
+from flask import jsonify, current_app
+from tables.dbModels import db
+from sqlalchemy import text as t
+
+@token_required
+def clear_google_token(current_user):
+    try:
+        if not current_user:
+            return jsonify({"Google token err": "You are not allowed to perform this operation. Login required"}), 403
+        with db.engine.connect() as connection:
+            clear_token = t("UPDATE user SET google_token = NULL WHERE public_id = : public_id")
+            affected_column = connection.execute(statement=clear_token, parameters={"public_id":current_user.public_id})
+            token = affected_column
+            connection.commit()
+            if token.rowcount == 0:
+                return jsonify({"token-column-err": "User not found in the database"}), 404
+            current_app.logger.info(f"Cleared google token for user: {current_user}")
+            return jsonify({"Cleared":"Token cleared"}), 200
+    except Exception as e:
+        connection.rollback()
+        current_app.logger.exception("Failed to clear google token from database")
+        return jsonify({"Clearing-token-err":str(e)}), 500
+    
