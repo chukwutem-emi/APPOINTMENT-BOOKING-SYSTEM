5a0d7d29d42453fb3ab1ff52ff6896535d1b2afd
chukwutem-emi <chukwutememi@gmail.com>
2025-07-06 16:45:50 +0100

    updated

--- src/routes/token/clearGoogleToken.py
+++ src/routes/token/clearGoogleToken.py
@@ -9,16 +9,15 @@ def clear_google_token(current_user):
         if not current_user:
             return jsonify({"Google token err": "You are not allowed to perform this operation. Login required"}), 403
         with db.engine.connect() as connection:
-            clear_token = t("UPDATE user SET google_token = NULL WHERE public_id = : public_id")
+            clear_token = t("UPDATE `user` SET google_token = NULL WHERE public_id=:public_id")
             affected_column = connection.execute(statement=clear_token, parameters={"public_id":current_user.public_id})
-            token = affected_column
+            result = affected_column
             connection.commit()
-            if token.rowcount == 0:
+            if result.rowcount == 0:
                 return jsonify({"token-column-err": "User not found in the database"}), 404
             current_app.logger.info(f"Cleared google token for user: {current_user}")
             return jsonify({"Cleared":"Token cleared"}), 200
     except Exception as e:
-        connection.rollback()
         current_app.logger.exception("Failed to clear google token from database")
         return jsonify({"Clearing-token-err":str(e)}), 500
     
