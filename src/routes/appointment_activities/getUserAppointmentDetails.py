d8388da22075e302b74eff433df915884a7316eb
chukwutem-emi <chukwutememi@gmail.com>
2025-08-23 12:51:03 +0100

    updated

--- src/routes/appointment_activities/getUserAppointmentDetails.py
+++ src/routes/appointment_activities/getUserAppointmentDetails.py
@@ -15,27 +15,23 @@ def a_user_appointment_details(current_user):
         with db.engine.connect() as connection:
             get_a_user_appointment_details = t("SELECT * FROM appointment WHERE user_id=:user_id")
 
-            user_appointments = connection.execute(statement=get_a_user_appointment_details, parameters={"user_id":current_user.id})
-            user = user_appointments.fetchall()
-            if len(user) == 0:
+            user_appointments = connection.execute(statement=get_a_user_appointment_details, parameters={"user_id":current_user.id}).fetchone()
+            if not user_appointments:
                 return jsonify({"user_appointment_not found":"The user appointment details that You are trying to access is not found!"}), 404
             
-            appointment_list = []
-            for appointment in user:
-                cleaned_row = {}
-                appointment_row = appointment._asdict()
-                for key, value in appointment_row.items():
-                    # Skip null values
-                    if value is None:
-                        continue   
-                    elif isinstance(value, timedelta):
-                        cleaned_row[key] = str(value)
-                    elif isinstance(value, datetime):
-                        cleaned_row[key] = value.isoformat()
-                    else:
-                        cleaned_row[key] = value
-                appointment_list.append(cleaned_row)
-            return jsonify({"user_appointments":appointment_list}), 200
+            # clean the row
+            appointment_dict = {}
+            for key, value in user_appointments._mapping.items():
+                # Skip null values
+                if value is None:
+                    continue   
+                elif isinstance(value, timedelta):
+                    appointment_dict[key] = str(value)
+                elif isinstance(value, datetime):
+                    appointment_dict[key] = value.isoformat()
+                else:
+                    appointment_dict[key] = value
+            return jsonify({"user_appointments":appointment_dict}), 200
         
     except dbError as d:
         return jsonify({"a_user_appointment_dbError":f"The server/database encountered an error. Please try again later!:{str(d)}"}), 500
