e0286c7bbc0f16bfe330d1d6bb3d136138a430ec
chukwutem-emi <chukwutememi@gmail.com>
2025-08-31 15:06:10 +0100

    updated

--- src/routes/personnel/__init__.py
+++ src/routes/personnel/__init__.py
@@ -3,7 +3,6 @@ from routes.personnel.createPersonnel import create_personnel
 from flask_cors import CORS
 from .deletePersonnel import delete_personnel
 from .getAllPersonnel import get_all_personnel
-from .getPersonnel import get_personnel
 from .updatePersonnel import update_personnel
 from routes.utils.constants import FRONT_END_URL
 
@@ -20,5 +19,4 @@ CORS(
 personnel_bp.add_url_rule(rule="/personnel", endpoint="personnel", view_func=create_personnel, methods=["POST", "OPTIONS"])
 personnel_bp.add_url_rule(rule="/delete-personnel", endpoint="/delete-personnel", view_func=delete_personnel, methods=["DELETE", "OPTIONS"])
 personnel_bp.add_url_rule(rule="/all-personnel", endpoint="all-personnel", view_func=get_all_personnel, methods={"GET", "OPTIONS"})
-personnel_bp.add_url_rule(rule="/one-personnel", endpoint="one-personnel", view_func=get_personnel, methods=["GET", "OPTIONS"])
 personnel_bp.add_url_rule(rule="/update-personnel", endpoint="update-personnel", view_func=update_personnel, methods=["PUT", "OPTIONS"])
