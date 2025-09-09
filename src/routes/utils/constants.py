175dcdab8e8c3157148da5d5dfa1ee48adda1a70
chukwutem-emi <chukwutememi@gmail.com>
2025-09-09 12:26:23 +0100

    updated

--- src/routes/utils/constants.py
+++ src/routes/utils/constants.py
@@ -1,4 +1,4 @@
 PAYSTACK_PAYMENT_API = "https://api.paystack.co/transaction/initialize"
 SCOPE = "https://www.googleapis.com/auth/calendar"
-FRONT_END_URL = "https://booksmart-ten.vercel.app/"
+FRONT_END_URL = "https://booksmart-ten.vercel.app"
 GOOGLE_REDIRECT_URI="https://appointment-booking-system-dgpm.onrender.com/api/bookApp/oauth2callback"
